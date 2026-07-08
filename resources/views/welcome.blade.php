f94370dc0f09c9dd827757dc82274b768139adac
durdona121307 <durdonautkirovna4@gmail.com>
2026-07-08 12:56:37 +0500

    Initial Laravel project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="uz">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HelpDesk Pro</title>
+
+    <style>
+        body{
+            margin:0;
+            font-family:Arial, Helvetica, sans-serif;
+            background:#0f172a;
+            color:white;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            height:100vh;
+        }
+
+        .card{
+            text-align:center;
+            background:#1e293b;
+            padding:50px;
+            border-radius:15px;
+            width:600px;
+            box-shadow:0 0 25px rgba(0,0,0,.3);
+        }
+
+        h1{
+            font-size:50px;
+            color:#3b82f6;
+        }
+
+        p{
+            color:#cbd5e1;
+            font-size:20px;
+        }
+
+        .btn{
+            margin-top:30px;
+            display:inline-block;
+            padding:15px 35px;
+            background:#2563eb;
+            color:white;
+            text-decoration:none;
+            border-radius:10px;
+            font-size:18px;
+        }
+
+        .btn:hover{
+            background:#1d4ed8;
+        }
+    </style>
+</head>
+<body>
+
+<div class="card">
+
+<h1>HelpDesk Pro</h1>
+
+<p>
+Professional IT Support Ticket System
+</p>
+
+<a href="#" class="btn">
+Get Started
+</a>
+
+</div>
+
+</body>
+</html>
